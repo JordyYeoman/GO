458dc42544410777b08978916aea789d8399e618
Jordy Yeoman <jordyyeoman@gmail.com>
2023-04-26 22:36:03 +0800

    chore: sharing packages in go

--- go-beginner/main.go
+++ go-beginner/main.go
@@ -1,1 +1,24 @@
-// Testing
+package main
+
+import (
+	"fmt"
+	"myapp/doctor"
+	"myapp/jarvis"
+)
+
+func main() {
+	//
+	sendItVar := "sendItVar"
+	fmt.Println("SEND ITTTT")
+	getSome(sendItVar)
+	//
+	// Testing external package
+	fmt.Println(doctor.Intro())
+	// doMethod := doctor.Intro()
+	// Testing external package 2
+	fmt.Println(jarvis.Intro())
+}
+
+func getSome(input string) {
+	fmt.Println(input)
+}
