f3535fed9757833bf96d115c598099190b3475cf
Jordy Yeoman <jordyyeoman@gmail.com>
2023-04-27 22:41:04 +0800

    chore: understanding switch cases

--- go-beginner/main.go
+++ go-beginner/main.go
@@ -1,24 +1,67 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"myapp/doctor"
 	"myapp/jarvis"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	//
-	sendItVar := "sendItVar"
-	fmt.Println("SEND ITTTT")
-	getSome(sendItVar)
-	//
-	// Testing external package
-	fmt.Println(doctor.Intro())
-	// doMethod := doctor.Intro()
-	// Testing external package 2
-	fmt.Println(jarvis.Intro())
+	testDoctor := doctor.Intro()
+	testJarvis := jarvis.Intro()
+
+	fmt.Println(testDoctor)
+	fmt.Println(testJarvis)
+
+	// Reader
+	reader := bufio.NewReader(os.Stdin)
+	
+	for {
+		fmt.Print("--> ")
+		// Take in user input and assign to variable
+		userInput, _ := reader.ReadString('\n')
+
+		userInput = strings.Replace(userInput, "\r\n", "", -1)
+		userInput = strings.Replace(userInput, "\n", "", -1)
+
+		if userInput == "quit" {
+			break
+		} else {
+			fmt.Println(doctor.Response(userInput))
+		}
+	}
+
+	// Testing switch statements
+	switchWithoutExpression()
+	switchWithExpression("test")
+
+	fmt.Println("Program terminated successfully.")
 }
 
-func getSome(input string) {
-	fmt.Println(input)
+func switchWithoutExpression() {
+	// A switch without a condition is the same as switch true.
+	t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("It's before noon")
+    default:
+        fmt.Println("It's after noon")
+    }
+}
+
+func switchWithExpression(input string) {
+	switch input {
+		case "yes":
+			fmt.Println("Yes match")
+		case "no":
+			fmt.Println("No match")
+		case "test":
+			fmt.Println("test match")
+		default:
+			fmt.Println("No cases match")
+	}
 }
