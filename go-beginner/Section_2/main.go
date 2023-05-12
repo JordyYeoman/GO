9c1e9958afdb87f3267fd3bc2f5b5b9ce6170c32
Jordy Yeoman <jordyyeoman@gmail.com>
2023-05-12 09:28:38 +0800

    feat: user input game

--- go-beginner/Section_2/main.go
+++ go-beginner/Section_2/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "time"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
 
 func runNTimes(n int) {
 	for i := 0; i < n; i++ {
@@ -9,9 +14,40 @@ func runNTimes(n int) {
 	}
 }
 
+const userInputPrompt = "and press enter when ready"
+
 func main() {
 	println("Say WHAT AGAIN MOTHERFKER")
 	println("I DARE YOU, I DOUBLE DARE YOU!")
-	runNTimes(3)
+	// runNTimes(3)
 	println("What?!")
+
+	firstNumber := 2
+	secondNumber := 7
+	subtraction := 3
+	var answer int
+
+	// Take user input
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Guess the number game!")
+	fmt.Println("================================================")
+	fmt.Println("")
+	fmt.Println("Guess a number between 1 and 10", userInputPrompt)
+	reader.ReadString('\n')
+
+	// Play game
+	fmt.Println("Multiply your number by ", firstNumber, userInputPrompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Multiply the result by ", secondNumber, userInputPrompt)
+	reader.ReadString('\n')
+
+	fmt.Println("Divide the result by your original number ", userInputPrompt)
+	reader.ReadString('\n')
+	
+	fmt.Println("Now subtract  ", subtraction, userInputPrompt)
+	reader.ReadString('\n')
+
+	// Return answer to user
 }
