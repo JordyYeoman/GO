feac76c3cfdd47d9955c388a86d8319fa324a2e2
Jordy Yeoman <jordyyeoman@gmail.com>
2023-05-12 10:16:27 +0800

    chore: adding untracked files

--- go-beginner/Go_Scraping/scraper.go
+++ go-beginner/Go_Scraping/scraper.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
 func main() {
+	println("Scraper setup is starting...")
+
+	c := colly.NewCollector()
 	
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 }
