a1b00cbe7518a5948585e999dce8ba887139e7ba
Jordy Yeoman <jordyyeoman@gmail.com>
2023-05-14 15:54:24 +0800

    feat: further scrape testing

--- go-beginner/Go_Scraping/scraper.go
+++ go-beginner/Go_Scraping/scraper.go
@@ -7,18 +7,27 @@ import (
 )
 
 func main() {
-    // siteUrl := "https://scrapeme.live/shop/"
-    siteUrl := "https://www.betfair.com.au/exchange/plus/australian-rules/market/1.213699333"
-    
+    siteUrl := "https://scrapeme.live/shop/"
 
     // initializing the slice of structs that will contain the scraped data 
-    // var pokemonProducts []PokemonProduct
+    var pokemonProducts []PokemonProduct
 
 	c := colly.NewCollector()
     c.UserAgent = "Go program"
     
-    c.OnHTML(".mv-runner-list", func(e *colly.HTMLElement) {
-        println("e:", e)
+    c.OnHTML("li.product", func(e *colly.HTMLElement) { 
+        // initializing a new PokemonProduct instance 
+        pokemonProduct := PokemonProduct{} 
+
+        // scraping the data of interest 
+        pokemonProduct.url = e.ChildAttr("a", "href") 
+        pokemonProduct.image = e.ChildAttr("img", "src") 
+        pokemonProduct.name = e.ChildText("h2") 
+        pokemonProduct.price = e.ChildText(".price") 
+
+        // adding the product instance with scraped data to the list of products 
+        pokemonProducts = append(pokemonProducts, pokemonProduct) 
+        
     })
 
     c.Visit(siteUrl)
@@ -60,19 +69,3 @@ func loggingSiteDetails(c *colly.Collector) {
 type PokemonProduct struct { 
 	url, image, name, price string 
 }
-
-
-// c.OnHTML("li.product", func(e *colly.HTMLElement) { 
-//     // initializing a new PokemonProduct instance 
-//     pokemonProduct := PokemonProduct{} 
-
-//     // scraping the data of interest 
-//     pokemonProduct.url = e.ChildAttr("a", "href") 
-//     pokemonProduct.image = e.ChildAttr("img", "src") 
-//     pokemonProduct.name = e.ChildText("h2") 
-//     pokemonProduct.price = e.ChildText(".price") 
-
-//     // adding the product instance with scraped data to the list of products 
-//     pokemonProducts = append(pokemonProducts, pokemonProduct) 
-    
-// })
