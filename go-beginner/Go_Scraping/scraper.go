f797e3a1d312007294df4654f948d5e98109ed61
Jordy Yeoman <jordyyeoman@gmail.com>
2023-05-14 11:31:10 +0800

    feat: send it

--- go-beginner/Go_Scraping/scraper.go
+++ go-beginner/Go_Scraping/scraper.go
@@ -7,28 +7,18 @@ import (
 )
 
 func main() {
-    // siteUrl := "https://www.betfair.com.au/exchange/plus/"
-    siteUrl := "https://scrapeme.live/shop/"
+    // siteUrl := "https://scrapeme.live/shop/"
+    siteUrl := "https://www.betfair.com.au/exchange/plus/australian-rules/market/1.213699333"
+    
 
     // initializing the slice of structs that will contain the scraped data 
-    var pokemonProducts []PokemonProduct
+    // var pokemonProducts []PokemonProduct
 
 	c := colly.NewCollector()
     c.UserAgent = "Go program"
     
-    c.OnHTML("#base > div > div.centeredContainer_f19gskyt > div > div.contentAndFooterContainer_fx60ys1.layoutContainersWhenNextRacesRibbonShown_f1k13kpz > div > div > div.childContentWithRibbonShown_fphioe2 > div > div > div > div > div.content_f12s7fql > div:nth-child(4) > div > div.carouselDesktop_f1f1ygnm > div:nth-child(1) > div > div > div > div > div.priceButtons_f1hf8rho > div:nth-child(1) > div.priceButtonContainer_fse8zei > div > div > div > button > div > div > div > span > div", func(e *colly.HTMLElement) { 
-        // initializing a new PokemonProduct instance 
-        pokemonProduct := PokemonProduct{} 
-    
-        // scraping the data of interest 
-        pokemonProduct.url = e.ChildAttr("a", "href") 
-        pokemonProduct.image = e.ChildAttr("img", "src") 
-        pokemonProduct.name = e.ChildText("h2") 
-        pokemonProduct.price = e.ChildText(".price") 
-    
-        // adding the product instance with scraped data to the list of products 
-        pokemonProducts = append(pokemonProducts, pokemonProduct) 
-        
+    c.OnHTML(".mv-runner-list", func(e *colly.HTMLElement) {
+        println("e:", e)
     })
 
     c.Visit(siteUrl)
@@ -70,3 +60,19 @@ func loggingSiteDetails(c *colly.Collector) {
 type PokemonProduct struct { 
 	url, image, name, price string 
 }
+
+
+// c.OnHTML("li.product", func(e *colly.HTMLElement) { 
+//     // initializing a new PokemonProduct instance 
+//     pokemonProduct := PokemonProduct{} 
+
+//     // scraping the data of interest 
+//     pokemonProduct.url = e.ChildAttr("a", "href") 
+//     pokemonProduct.image = e.ChildAttr("img", "src") 
+//     pokemonProduct.name = e.ChildText("h2") 
+//     pokemonProduct.price = e.ChildText(".price") 
+
+//     // adding the product instance with scraped data to the list of products 
+//     pokemonProducts = append(pokemonProducts, pokemonProduct) 
+    
+// })
