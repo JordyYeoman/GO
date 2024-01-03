f41652928ca8689662771fb0ccfc9d6aa4871b2c
Jordy Yeoman <jordyyeoman@gmail.com>
2024-01-03 23:09:51 +0800

    feat: initial setup for project and basic routes

--- go-htmx/main.go
+++ go-htmx/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("Server Starting...")
 
-	
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World\n")
+		io.WriteString(w, r.Method)
+	}
+	http.HandleFunc("/", h1)
+
+	fmt.Println("Server running at Port:8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
