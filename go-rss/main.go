af406a8f7aa2c3af00b14fa0ae94e54237b4e5cf
Jordy Yeoman <jordyyeoman@gmail.com>
2024-02-01 06:26:41 +0800

    feat: initial pieces

--- go-rss/main.go
+++ go-rss/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	fmt.Println("Server starting...")
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("PORT:", port)
 }
