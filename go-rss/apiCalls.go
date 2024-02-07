f7b0eb4b3c3a0b0f1827f8a984ebc35ee4ddec2d
Jordy Yeoman <jordyyeoman@gmail.com>
2024-02-07 21:15:59 +0800

    feat: simple http request

---
+++ go-rss/apiCalls.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handleApiCall() {
+	url := os.Args[1]
+
+	api(url)
+}
+
+func api(url string) {
+	response, err := http.Get(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// read response body
+	body, error := io.ReadAll(response.Body)
+	if error != nil {
+		fmt.Println(error)
+	}
+	// close response body
+	response.Body.Close()
+
+	// print response body
+	fmt.Println(string(body))
+}
--- go-rss/main.go
+++ go-rss/main.go
@@ -1,77 +1,96 @@
 package main
 
 import (
-	"database/sql"
+	// "database/sql"
 	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	_ "github.com/go-sql-driver/mysql" // Importing a package for side effects, no direct usages (interface for DB)
-	"github.com/joho/godotenv"
+	// "log"
+	// "net/http"
+	// "os"
+	// "github.com/go-chi/chi"
+	// "github.com/go-chi/cors"
+	// _ "github.com/go-sql-driver/mysql" // Importing a package for side effects, no direct usages (interface for DB)
+	// "github.com/joho/godotenv"
 )
+
+func newCard() string {
+	return "Five of diamonds"
+}
 
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println("Server starting...")
 
-	godotenv.Load()
+	card := newCard()
+	fmt.Println(card)
 
-	port := os.Getenv("PORT")
-	db_url := os.Getenv("DB_URL")
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, "Ace of Spades")
 
-	fmt.Println("Connecting to DB:")
-	db, dbErr := sql.Open("mysql", db_url)
-	if dbErr != nil {
-		log.Fatal(dbErr)
+	for i, card := range cards {
+		fmt.Println(i, card)
 	}
 
-	if dbErr = db.Ping(); dbErr != nil {
-		log.Fatal(dbErr)
-	}
+	handleApiCall()
 
-	// Working with SQL examples
-	handleSQLExampleQueries(db)
+	// Array - fixed list of things
+	// Slice - An array that can grow or shrink (dynamic arr)
+	// Every element of a slice must be of identical type
 
-	defer db.Close() // Defer means run this when the wrapping function terminates
+	// godotenv.Load()
 
-	if port == "" {
-		log.Fatal("PORT is not found in the environment")
-	}
+	// port := os.Getenv("PORT")
+	// db_url := os.Getenv("DB_URL")
 
-	router := chi.NewRouter()
+	// fmt.Println("Connecting to DB:")
+	// db, dbErr := sql.Open("mysql", db_url)
+	// if dbErr != nil {
+	// 	log.Fatal(dbErr)
+	// }
 
-	router.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
+	// if dbErr = db.Ping(); dbErr != nil {
+	// 	log.Fatal(dbErr)
+	// }
 
-	v1Router := chi.NewRouter()
+	// // Working with SQL examples
+	// handleSQLExampleQueries(db)
 
-	// v1Router.HandleFunc("/healthz", handlerReadiness) // Any endpoint scoping
-	v1Router.Get("/healthz", handlerReadiness) // Scopes the route to only GET requests
-	v1Router.Get("/err", handlerError)
+	// defer db.Close() // Defer means run this when the wrapping function terminates
 
-	router.Mount("/v1", v1Router)
+	// if port == "" {
+	// 	log.Fatal("PORT is not found in the environment")
+	// }
 
-	srv := &http.Server{
-		Handler: router,
-		Addr:    ":" + port,
-	}
+	// router := chi.NewRouter()
 
-	log.Printf("Server starting on PORT: %v", port)
-	err := srv.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	// router.Use(cors.Handler(cors.Options{
+	// 	// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+	// 	AllowedOrigins: []string{"https://*", "http://*"},
+	// 	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+	// 	AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	// 	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	// 	ExposedHeaders:   []string{"Link"},
+	// 	AllowCredentials: false,
+	// 	MaxAge:           300, // Maximum value not ignored by any of major browsers
+	// }))
 
-	fmt.Println("PORT:", port)
+	// v1Router := chi.NewRouter()
+
+	// // v1Router.HandleFunc("/healthz", handlerReadiness) // Any endpoint scoping
+	// v1Router.Get("/healthz", handlerReadiness) // Scopes the route to only GET requests
+	// v1Router.Get("/err", handlerError)
+
+	// router.Mount("/v1", v1Router)
+
+	// srv := &http.Server{
+	// 	Handler: router,
+	// 	Addr:    ":" + port,
+	// }
+
+	// log.Printf("Server starting on PORT: %v", port)
+	// err := srv.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println("PORT:", port)
 }
