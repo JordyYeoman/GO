51c3df1d36b2285da5b708d222a349ca1ec5d508
Jordy Yeoman <jordyyeoman@gmail.com>
2024-03-10 12:20:28 +0800

    feat: practice qs 1

--- ToddMcleod_Go_Course_Udem/main.go
+++ ToddMcleod_Go_Course_Udem/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Full Send")
@@ -11,8 +14,11 @@ func main() {
 	fmt.Printf("%T\n", x)
 	fmt.Printf("%T\n", y)
 
-	// Practice Questions #1
+	// Practice Questions
+	// #1
 	handsOnQuestions1()
+	// #2
+	handsOnQuestions2()
 }
 
 type Square struct {
@@ -21,12 +27,29 @@ type Square struct {
 	units  string
 }
 
-// Here we are assigning the function to a type using a 'receiver'.
-func (s Square) getAreaForShape() float64 {
-	return s.width * s.length
+type Circle struct {
+	radius float64
+	units  string
 }
 
-type Circle struct {
+// Shape - Encompassing all shapes into an interface,
+// they require the 'area()' method.
+type Shape interface {
+	area() float64
+}
+
+// Using a receiver to attach a method to the Shape interface.
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * (c.radius * c.radius)
+}
+
+// Here we are assigning the function to a type using a 'receiver'.
+func (s Square) area() float64 {
+	return s.width * s.length
 }
 
 func handsOnQuestions1() {
@@ -37,5 +60,57 @@ func handsOnQuestions1() {
 	}
 
 	// Calling the function attached to the type
-	fmt.Printf("Area of your square is %+v%+v\n", testSquare.getAreaForShape(), testSquare.units)
+	fmt.Printf("Area of your square is %+v%+v\n", testSquare.area(), testSquare.units)
+
+	testCircle := Circle{
+		10,
+		"mm",
+	}
+
+	// Calling the function attached to the type
+	fmt.Printf("Area of your circle is %+v%+v\n", math.Round(testCircle.area()*100)/100, testCircle.units)
+
+	// Now we have created the interface for all 'Shapes' and attached the method using a receiver, we can
+	// simply call that method and pass in our 'Shape' to get the area.
+	fmt.Printf("Area of circle is %+v\n", getArea(testCircle))
+	fmt.Printf("Area of square is %+v\n", getArea(testSquare))
+}
+
+type Person struct {
+	firstName string
+	lastName  string
+}
+
+type SecretAgent struct {
+	Person
+}
+
+func (p Person) pSpeak() {
+	fmt.Printf("pSpeak\n")
+}
+
+func (s SecretAgent) saSpeak() {
+	fmt.Printf("saSpeak\n")
+}
+
+func handsOnQuestions2() {
+	p := Person{
+		"Amara",
+		"Yeoman",
+	}
+	secretAgent := SecretAgent{
+		Person{
+			"Jordy",
+			"Yeoman",
+		},
+	}
+
+	fmt.Println(p.firstName)
+	fmt.Println(secretAgent.firstName)
+	// Run speak method of Person type
+	p.pSpeak()
+	// Run speak method of SecretAgent type
+	secretAgent.pSpeak()        // Works
+	secretAgent.Person.pSpeak() // Also works - more verbose?
+	secretAgent.saSpeak()
 }
