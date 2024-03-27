a30d75d3cded5ca15fb53b78c6f3ac8b9e69a4a2
Jordy Yeoman <jordyyeoman@gmail.com>
2024-03-27 21:43:03 +0800

    feat: composing templates in go 3

--- ToddMcleod_Go_Course_Udem/templates/lesson_09/main.go
+++ ToddMcleod_Go_Course_Udem/templates/lesson_09/main.go
@@ -1,1 +1,66 @@
-package lesson_09
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type Player struct {
+	Name         string
+	Age          int
+	AvgDisposals float64
+}
+
+type Team struct {
+	Players []Player
+	Name    string
+}
+
+var tpl *template.Template
+
+func doubleIt(n float64) float64 {
+	return n * 2
+}
+
+func squareNum(n float64) float64 {
+	return n * n
+}
+
+var fm = template.FuncMap{
+	"double": doubleIt,
+	"sqr":    squareNum,
+}
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("tpl.gohtml"))
+}
+
+func main() {
+	p1 := Player{
+		"Jordy",
+		29,
+		27.7,
+	}
+	p2 := Player{
+		"Sean",
+		29,
+		14.2,
+	}
+	p3 := Player{
+		"Luke",
+		29,
+		29.2,
+	}
+	team := Team{
+		Players: []Player{
+			p1, p2, p3,
+		},
+		Name: "Parkerville",
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", team)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
