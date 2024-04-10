84c848e24bbefff0f725474c8317d2603ef19d7b
Jordy Yeoman <jordyyeoman@gmail.com>
2024-04-10 22:04:17 +0800

    feat: net/http pkg with request form

--- ToddMcleod_Go_Course_Udem/http-pkg/lesson_01/main.go
+++ ToddMcleod_Go_Course_Udem/http-pkg/lesson_01/main.go
@@ -2,27 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"text/template"
 )
 
 // Important!!
 // type handler interface {
-//	ServeHttp(ResponseWriter, Request)
+//	ServeHttp(ResponseWriter, *Request)
 // }
 
 type hotdog int
 
 // Extending the above hotdog type to include
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatal("Could not parse form: ", err)
+	}
+
+	fmt.Println(r.Form)
+
+	err = tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+	if err != nil {
+		log.Fatal("Unable to execute template.")
+	}
 	fmt.Fprintf(w, "Any code you want in this cheeky bugger")
 }
 
-func main() {
-	fmt.Println("Yo dawg whats up!!")
+var tpl *template.Template
 
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
 	var d hotdog
 
-	http.ListenAndServe(":8080", d)
+	err := http.ListenAndServe(":8080", d)
+	if err != nil {
+		log.Fatal("Server Died")
+	}
 
 	return
 }
