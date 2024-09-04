591f444965fcc2b08d473dd8a7133fadc2414d47
Jordy Yeoman <jordyyeoman@gmail.com>
2024-09-04 20:49:02 +0800

    feat: add path

--- ToddMcleod_Go_Course_Udem/docker-builds/builds-go/main.go
+++ ToddMcleod_Go_Course_Udem/docker-builds/builds-go/main.go
@@ -8,6 +8,8 @@ import (
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/t", t)
+
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Fatal("unable to start server")
@@ -16,6 +18,13 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	_, err := io.WriteString(w, "Hello from test go build - inside a docker container")
+	if err != nil {
+		log.Println("unable to write string")
+	}
+}
+
+func t(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, "Welcome to the /t path. Lets rock.")
 	if err != nil {
 		log.Println("unable to write string")
 	}
